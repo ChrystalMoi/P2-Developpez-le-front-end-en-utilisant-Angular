001bdf90af160c29737d411c4ab916730ff9044f
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-11-10 14:55:53 +0100

    add component detail with 'ng generate component detail'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ChartsModule } from 'ng2-charts';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, DetailComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent],
