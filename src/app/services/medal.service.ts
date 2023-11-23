7ec3a88792f58b778e3e925638b52c735fa531f9
Chrystal Bastid <chrystal.bastid@gmail.com>
2023-11-23 15:43:30 +0100

    add comments in service MedalService

--- src/app/services/medal.service.ts
+++ src/app/services/medal.service.ts
@@ -5,18 +5,23 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 
+// Service responsable de la gestion des médailles
 export class MedalService {
+  // Pour stocker le nombre total de médailles par participation
   private static totalMedalsByParticipation: number = 0;
 
-  // Utilisation d'un Subject pour gérer les médailles
+  // Utilisation d'un Subject pour gérer les observateur des changements de médaille
   private medalCountSubject = new Subject<number>();
 
-  // Observable pour que les composants puissent s'abonner aux changements de médailles
+  // Observable pour que les composant puissent s'abonner aux changements de medal
   medalCount$ = this.medalCountSubject.asObservable();
 
   // Méthode pour mettre à jour le nombre de médailles
   updateMedalCount(count: number) {
+    // Ajoute le nombre de médailles passer en paramètre au total
     MedalService.totalMedalsByParticipation += count;
+
+    // Notifie les observateurs du changement de nombre de médailles
     this.medalCountSubject.next(MedalService.totalMedalsByParticipation);
   }
 
@@ -25,6 +30,6 @@ export class MedalService {
     return MedalService.totalMedalsByParticipation;
   }
 
-
+  // Constructeur vide car il n'y a pas d'initialisation spécifique nécessaire ici
   constructor() { }
 }
